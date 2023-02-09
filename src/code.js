778dae012658640c71fc06172c22cab5d9c5cd4c
reeveboy <reeveboy10@gmail.com>
2023-02-09 22:39:46 +0530

    new logo + gaming

--- src/code.js
+++ src/code.js
@@ -38,12 +38,12 @@ function gotoschedule() {
     .scrollIntoView({ behavior: "smooth", block: "start" });
 }
 
-document.onreadystatechange = function () {
-  if (document.readyState !== "complete") {
-    document.querySelector("body").style.visibility = "hidden";
-    document.querySelector("#loader").style.visibility = "visible";
-  } else {
-    document.querySelector("#loader").style.display = "none";
-    document.querySelector("body").style.visibility = "visible";
-  }
-};
+// document.onreadystatechange = function () {
+//   if (document.readyState !== "complete") {
+//     document.querySelector("body").style.visibility = "hidden";
+//     document.querySelector("#loader").style.visibility = "visible";
+//   } else {
+//     document.querySelector("#loader").style.display = "none";
+//     document.querySelector("body").style.visibility = "visible";
+//   }
+// };
