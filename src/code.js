777735d5d3c6308eab68b5fd8a896eace30f5df8
reeveboy <reeveboy10@gmail.com>
2023-02-09 09:11:12 +0530

    Loading

--- src/code.js
+++ src/code.js
@@ -37,3 +37,13 @@ function gotoschedule() {
     .getElementById("schedule")
     .scrollIntoView({ behavior: "smooth", block: "start" });
 }
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
